559c0b542d5620c92d60a192c052c9bd7041145b
Hakob Aghajanyan <haaghaja@c1r5s3.42yerevan.am>
2026-04-30 18:33:24 +0400

    feat: react setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>ft_transcendence</h1>
+      <p>React is working</p>
+      <button>Click me</button>
+    </div>
+  )
+}
+
+export default App
